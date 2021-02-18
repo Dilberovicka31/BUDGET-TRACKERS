98090f5d20a0fa8bc4a0fd617def6dd4dbeebaad
Dilberovicka31 <dilberovicm36@gmail.com>
2021-02-17 21:37:31 -0800

    fixed server.js

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/enigmatic-dusk", {
+mongoose.connect(process.env.MONGODB_URI ||"mongodb://localhost/enigmatic-dusk", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
